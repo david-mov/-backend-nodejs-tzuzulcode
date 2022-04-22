4fedfb8ba6e86b4d2d580689ede3fee5b228208e
David <83718324+david-mov@users.noreply.github.com>
2022-04-22 09:50:08 -0300

    github test commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/',(req, res) => {
-    res.json('Hello World!')
+    res.status(200).json('Hello World!')
 })
 
 app.listen(port, () => {
