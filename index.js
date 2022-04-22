e52afe607a020fe4c5c840bd36436ef231664e06
David <83718324+david-mov@users.noreply.github.com>
2022-04-22 20:15:18 -0300

    apirestful example

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 const {env,port} = require('./config')
 const express = require('express')
+
+const users = [] // bd example
 const app = express()
 
+app.use(express.json()) // middleware to use body JSON
+
 app.get('/',(req, res) => {
-    res.status(200).json('Hello World!')
+    enabledUsers = users.filter((user) => user.enabled)
+    return res.status(200).json(enabledUsers)
+})
+
+app.post('/', (req, res) => {
+    users.push({...req.body, id: users.length, enabled: true})
+    return res.status(201).json(users[users.length-1])
+})
+
+app.put('/:id', (req, res) => {
+    const {params:{id}, body} = req
+    console.log(id, body)
+    if (!users[id]) return res.status(400).json('Error: provided id is non-existent')
+    const {enabled} = users[id]
+    users[id] = {...users[id], body, id: id, enabled: enabled}
+    return res.status(201).json(users[id])
+})
+
+app.delete('/:id', (req, res) => {
+    const id = req.params.id
+    if (!users[id]) return res.status(400).json('Error: provided id is non-existent')
+    users[id] = {...users[id], enabled: false}
+    return res.status(201).json(users[id])
 })
 
 app.listen(port, () => {
