b8dc221520550b49d899e4a8ab9ae473faab63a6
David <83718324+david-mov@users.noreply.github.com>
2022-04-27 22:14:31 -0300

    mongoose connection

---
+++ job-search-app/config/db.js
@@ -0,0 +1,9 @@
+const { dbUsername, dbPassword, dbHost, dbName } = require('./')
+const mongoose = require('mongoose')
+
+const connection = async () => {
+    const conn = await mongoose.connect(`mongodb+srv://${dbUsername}:${dbPassword}@${dbHost}/${dbName}?retryWrites=true&w=majority`)
+    console.log("Mongo DB connected: ",conn.connection.host)
+}
+
+module.exports = {connection, mongoose}
--- job-search-app/config/index.js
+++ job-search-app/config/index.js
@@ -2,7 +2,11 @@ require("dotenv").config()
 
 const config = {
     env: process.env.NODE_ENV,
-    port: process.env.PORT || 4000
+    port: process.env.PORT,
+    dbUsername: process.env.DB_USERNAME,
+    dbPassword: process.env.DB_PASSWORD,
+    dbHost: process.env.DB_HOST,
+    dbName: process.env.DB_NAME,
 }
 
 module.exports = config
