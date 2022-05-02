557a1694c358565890a042edbe4d50ac17449185
David <83718324+david-mov@users.noreply.github.com>
2022-05-02 06:04:04 -0300

    fix fields email and password not required

--- job-search-app/models/index.js
+++ job-search-app/models/index.js
@@ -1,13 +1,11 @@
 const {mongoose: {Schema, model}} = require('../config/db')
 
-const userSchema = new Schema({
-    firstName: String,
-    lastName: String,
-    email: String,
-    password: String
-})
-
-const User = model('User', userSchema)
+const User = model('User', new Schema({
+    firstName: { type: String },
+    lastName: { type: String },
+    email: { type: String, required: true },
+    password: { type: String, required: true }
+}))
 
 module.exports = {
     User
