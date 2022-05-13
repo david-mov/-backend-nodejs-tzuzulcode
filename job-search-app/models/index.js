72629d9a74f2503693236a37fa0ca32fd588da48
David <83718324+david-mov@users.noreply.github.com>
2022-05-13 03:53:25 -0300

    improved user model and job model

--- job-search-app/models/index.js
+++ job-search-app/models/index.js
@@ -1,12 +1,67 @@
 const {mongoose: {Schema, model}} = require('../config/db')
 
-const User = model('User', new Schema({
-    firstName: { type: String },
-    lastName: { type: String },
+const LocationSchema = new Schema({
+    country: {type: String, required: true},
+    postalCode: {type: String}
+})
+
+const ContactSchema = new Schema({
+    website: {type: String},
+    phone: {type: String}
+})
+
+const EducationSchema = new Schema({
+    institution: {type: String, required: true},
+    title: {type: String},
+    discipline: {type: String},
+    description: {type: String},
+    startDate: {type: Date},
+    endDate: {type: Date}
+})
+
+const ExperienceSchema = new Schema({
+    position: {type: String, required: true},
+    company: {type: String, required: true},
+    jobType: {type: String, enum: ['fulltime', 'partime', 'flexible', 'autonomous', 'intership', 'temporal']},
+    laboralSector: {type: String},
+    country: {type: String},
+    currentlyActive: {type: Boolean, required: true},
+    startDate: {type: Date, required: true},
+    endDate: {type: Date}
+})
+
+const ProfileSchema = new Schema({
+    laboralSector: {type: String, required: true},
+    education: {type: EducationSchema, required: true},
+    experience: {type: ExperienceSchema}
+})
+
+
+const UserModel = model('User', new Schema({
+    fullName: { type: String, required: true},
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    location: {type: LocationSchema, required: true},
+    contact: {type: ContactSchema},
+    permissionLevel: {type: Number, required: true},
+    profile: {type: ProfileSchema}
+
+}))
+
+const JobModel = model('Job', new Schema({
+    name: {type: String, required: true},
+    sector: {type: String, required: true},
+    jobType: {
+        type: String, 
+        enum: ['fulltime', 'partime', 'flexible', 'autonomous', 'intership', 'temporal'], 
+        required: true
+    },
+    description: {type: String},
+    applicants: {type: Array, required: true},
+    owner: {type: String, required: true}
 }))
 
 module.exports = {
-    User
+    UserModel,
+    JobModel
 }
